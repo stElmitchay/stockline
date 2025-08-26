947aa6b98766b0d28317fffe4039aa2e37409e52
stElmitchay <elmitchay@gmail.com>
2025-08-26 10:43:31 +0000

    fix:added a stats component in wallet page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	webpack: (config) => {
 		config.externals["@solana/web3.js"] = "commonjs @solana/web3.js";
 		config.externals["@solana/spl-token"] = "commonjs @solana/spl-token";
